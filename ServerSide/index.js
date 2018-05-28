2177efd005b7f122d2825e9f6f5eef194219b5c9
massimilianoenea <massimilianoenea3@gmail.com>
2018-05-28 18:22:48 +0200

    Inserimento di httpdispatcher tra i moduli di nodejs e sistemazione del problema di cross domain in localhost

--- ServerSide/index.js
+++ ServerSide/index.js
@@ -1,9 +1,21 @@
 var http = require('http');
+var HttpDispatcher = require('../node_modules/httpdispatcher/httpdispatcher');
+var dispatcher = new HttpDispatcher();
 
-var server = http.createServer(function (req,res) {
+dispatcher.setStatic('/resources');
+dispatcher.setStaticDirname('static');
+
+dispatcher.onGet("/page1", function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Page One');
+});
+
+http.createServer(function (req,res) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
+    res.setHeader("Access-Control-Allow-Methods", "GET, PUT, POST");
     res.writeHead(200,{'Content-Type': 'text/plain'});
     res.end('Hello World');
-})
+}).listen(1337,'127.0.0.1');
 
-server.listen(1337,'127.0.0.1');
 console.log('listen on 8000');
