d448281901cb69c3b18080a2b229386e965fdfc8
massimilianoenea <36633878+massimilianoenea@users.noreply.github.com>
2018-06-21 23:07:50 +0200

    Update setup.js

--- ServerSide/modules/myModules/dbconnection/setup.js
+++ ServerSide/modules/myModules/dbconnection/setup.js
@@ -1,18 +1,8 @@
 var mysql = require ("mysql");
-
-if(process.env.PORT){
-   var con = mysql.createPool({
+var con = mysql.createPool({
         host: process.env.DATABASE_HOST,
         user: process.env.DATABASE_USER,
         password: process.env.DATABASE_PASSWORD,
         database: process.env.DATABASE_DB
     });
-}else{
-  var con = mysql.createPool({
-        host: "localhost",
-        user: "root",
-        password: "JuventusStadium1996",
-        database: "playaround"
-    });
-};
 module.exports = exports = con;
