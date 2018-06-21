bde896b823fa74663ac19a30979af554cc155899
massimilianoenea <36633878+massimilianoenea@users.noreply.github.com>
2018-06-21 17:22:23 +0200

    Update setup.js

--- ServerSide/modules/myModules/dbconnection/setup.js
+++ ServerSide/modules/myModules/dbconnection/setup.js
@@ -1,9 +1,18 @@
 var mysql = require ("mysql");
 
-var con = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "JuventusStadium1996",
-    database: "playaround"
-});
+if(process.env.PORT){
+   var con = mysql.createPool({
+        host: process.env.DATABASE_HOST,
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_DB
+    });
+}else{
+  var con = mysql.createPool({
+        host: "localhost",
+        user: "root",
+        password: "JuventusStadium1996",
+        database: "playaround"
+    });
+};
 module.exports = exports = con;
